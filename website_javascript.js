ab60a186802e4c861c6d9fd5e9d73f824cfe875b
unknown <farida-l>
2019-08-02 10:36:20 -0400

    initial commit

---
+++ website_javascript.js
@@ -0,0 +1,7 @@
+function change_div_color() {
+  document.getElementById("change_color").style.opacity= "0.0" ;
+}
+
+function bring_back() {
+  document.getElementById("change_color").style.opacity= "1.0" ;
+}
